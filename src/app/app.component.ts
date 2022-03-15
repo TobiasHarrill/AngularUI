20804fd258394e4e9450e00dd0f65d6d479c68f7
TobiasHarrill <69198465+TobiasHarrill@users.noreply.github.com>
2022-03-15 13:56:56 -0400

    inital

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControlName, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'AngularUI';
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+) { }
+
+ngOnInit() {
+  this.form = this.formBuilder.group({
+
+  });
+}
+
 }
